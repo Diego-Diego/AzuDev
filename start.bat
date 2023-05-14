e758737514b9a0e4c4391c369e86a6d83227683b
Diegohbss <107653207+Diegohbss@users.noreply.github.com>
2023-05-14 19:46:53 +0200

    First push

---
+++ start.bat
@@ -0,0 +1,2 @@
+cd /d C:\Users\diego\Desktop\AzuDev\server-data
+C:\Users\diego\Desktop\AzuDev\artifact\FXServer.exe +exec server.cfg +set onesync on +set onesync_enableInfinity 1 +set onesync_enableBeyond 1 +set onesync_population true
